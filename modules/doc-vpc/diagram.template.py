eed0151879226a9d5e86ef3b1e6e3ccd0a67ccf4
Damien Carpy <47356096+DamienCarpy@users.noreply.github.com>
2022-11-22 20:33:46 +0100

    Add diagrams + add VPC in diagram

--- modules/doc-vpc/diagram.template.py
+++ modules/doc-vpc/diagram.template.py
@@ -8,20 +8,22 @@ with Diagram("${name}", show=False, direction="TB"):
 
     with Cluster("${region}"):
 
+        with Cluster("${name} - ${vpc_cidr_block}"):
+
 %{ for az in reverse(azs) ~}
-        with Cluster("${az}"):
-            with Cluster("public"):
+            with Cluster("${az}"):
+                with Cluster("public"):
 %{ if length(public_subnets_cidr_blocks) != 0 }
-                subnet_${trimprefix(az,region)} = Subnet("${element(public_subnets_cidr_blocks,index(azs,az))}")
+                    subnet_${trimprefix(az,region)} = Subnet("${element(public_subnets_cidr_blocks,index(azs,az))}")
 %{ else ~}%{ if length(public_subnets_ipv6_cidr_blocks) != 0 }
-                subnet_${trimprefix(az,region)} = Subnet("${element(public_subnets_ipv6_cidr_blocks,index(azs,az))}")
+                    subnet_${trimprefix(az,region)} = Subnet("${element(public_subnets_ipv6_cidr_blocks,index(azs,az))}")
 %{ else ~}%{ endif ~}%{ endif ~}
 
-            with Cluster("private"):
+                with Cluster("private"):
 %{ if length(private_subnets_cidr_blocks) != 0 }
-                subnet_${trimprefix(az,region)} = Subnet("${element(private_subnets_cidr_blocks,index(azs,az))}")
+                    subnet_${trimprefix(az,region)} = Subnet("${element(private_subnets_cidr_blocks,index(azs,az))}")
 %{ else ~}%{ if length(private_subnets_ipv6_cidr_blocks) != 0 }
-                subnet_${trimprefix(az,region)} = Subnet("${element(private_subnets_ipv6_cidr_blocks,index(azs,az))}")
+                    subnet_${trimprefix(az,region)} = Subnet("${element(private_subnets_ipv6_cidr_blocks,index(azs,az))}")
 %{ else ~}%{ endif ~}%{ endif ~}
 
 %{ endfor ~}
